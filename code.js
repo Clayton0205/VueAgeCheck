62b6a9597a96337983908b5d9b3ac5ec9b91c2a7
Clayton0205 <claytonadonis5@gmail.com>
2022-07-04 14:39:50 +0200

    she said she was 12

---
+++ code.js
@@ -0,0 +1,24 @@
+const app = Vue.createApp({
+    data: function(){
+        return{
+            name: ' ',
+            age: ' '
+        }
+    },
+    methods:{
+        validate(){
+            if (this.age < 17){
+                alert('you have to enter your details.');
+            } else {
+                if (this.name = ' '){
+                    alert('Please enter your name')
+                } else {
+                    document.querySelector('#output').innerHTML = 
+                    `
+                    ${this.name} ${this.age}
+                    `
+                }} 
+            }}
+        })
+
+app.mount('#container')
